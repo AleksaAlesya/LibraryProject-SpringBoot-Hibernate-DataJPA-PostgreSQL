bb316a1ddd9ea5dd8221760fd580a930750eb459
AleksaBrakor <a.n.brakor@gmail.com>
2025-03-02 20:37:30 +0300

    03: AOP добавлено логирование всех методов в папке сервисы

---
+++ src/main/java/by/aleksabrakor/libraryProject/util/LoggingAspect.java
@@ -0,0 +1,23 @@
+package by.aleksabrakor.libraryProject.util;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    //    @Before("execution(* by.aleksabrakor.libraryProject.*.*.*(..))") //все методы
+    @Before("execution(* by.aleksabrakor.libraryProject.services.*.*(..))") // все методы сервиса
+    public void logServiceMethods(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();  // Получаем имя метода
+        String className = joinPoint.getTarget().getClass().getSimpleName(); // Получаем имя класса
+        logger.info("Вызван метод: {}.{}", className, methodName);
+    }
+}
